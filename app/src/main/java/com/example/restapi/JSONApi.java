46c852061bb4a76e331b556167dc16d8fe2e6878
Ashar Ashfaq <ashar.ashfaq2@gmail.com>
2019-06-08 14:49:03 +0500

    All Get Methods learned.

--- app/src/main/java/com/example/restapi/JSONApi.java
+++ app/src/main/java/com/example/restapi/JSONApi.java
@@ -1,12 +1,54 @@
 package com.example.restapi;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JSONApi {
 
-    @GET("posts")
-    Call<List<Post>> getPosts();
+    // https://jsonplaceholder.typicode.com/posts
+    /*@GET("posts")
+    Call<List<Post>> getPosts();*/
+
+
+    // https://jsonplaceholder.typicode.com/posts?userId=1
+    /*@GET("post")
+    Call<List<Post>> getPosts(@Query("userid") int userid);*/
+
+
+
+    // https://jsonplaceholder.typicode.com/posts?userId=1&_sort=id&_order=desc
+    // if we dont want to use any of the below parameters we simple pass null
+    //Difference between int and Integer is Integer takes null.
+    //if we want to have to 2 or more users we simple write another userid query.
+    //or better we can use simple Integer Array or list.
+    @GET("post")
+    Call<List<Post>> getPosts(
+            @Query("userid") Integer userid,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    //We can also use QueryMap if we dont know what would be the key and value.
+    //preferable is map
+    @GET("post")
+    Call<List<Post>> getPosts(@QueryMap Map<String,String> parameters);
+
+
+    // https://jsonplaceholder.typicode.com/posts/1/comments
+    @GET("posts/{id}/comments")
+    Call<List<Comments>> getComments(@Path("id") int postid);
+
+    //url = post/3/comments
+    @GET
+    Call<List<Comments>> getComments(@Url String url);
+
+
 }
--- app/src/main/java/com/example/restapi/MainActivity.java
+++ app/src/main/java/com/example/restapi/MainActivity.java
@@ -18,6 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    JSONApi jsonApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +28,15 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        jsonApi = retrofit.create(JSONApi.class);
 
-        JSONApi jsonApi = retrofit.create(JSONApi.class);
+        getPost();
+
+
+    }
+
+    private void getPost(){
+
         Call<List<Post>> call = jsonApi.getPosts();
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -52,6 +60,36 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getComments(){
+        Call<List<Comments>> call = jsonApi.getComments(3);
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(Call<List<Comments>> call, Response<List<Comments>> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Code "+ response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else{
+                    List<Comments> commentList = response.body();
+                    for(int i = 0; i < commentList.size(); i++){
+                        String content = "";
+                        content = "postid: "+commentList.get(i).getPostId()+"\n"
+                                +"id: "+ commentList.get(i).getId()+"\n"
+                                +"name: "+ commentList.get(i).getName()+"\n"
+                                +"Email: "+ commentList.get(i).getEmail()+"\n"
+                                +"body: "+ commentList.get(i).getText()+"\n"+"\n";
+                        textView.append(content);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comments>> call, Throwable t) {
                 Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
